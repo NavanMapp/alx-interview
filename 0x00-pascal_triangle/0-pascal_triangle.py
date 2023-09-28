92cf93ba59d2df48d7270a03024dc06c386735b6
Navan Maphalala [IT - Curro St Dominics - All Schools] <adm-navan.m@curro.co.za>
2023-09-28 11:43:32 +0200

    0x00-pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+'''
+Create a function def pascal_triangle(n): that returns a list of lists of 
+integers representing the Pascals triangle of n
+''' 
+
 def pascal_triangle(n):
     if n <= 0:
         return []
