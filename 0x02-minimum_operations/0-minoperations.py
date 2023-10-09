8f50590c2fb7fc0d23b521d47ddc1653aee70a5b
NavanMapp <navanmapp@gmail.com>
2023-10-09 12:26:27 +0000

    0x02-minimum_operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        operations[i] = float('inf')
+        for j in range(1, i):
+            if i % j == 0:
+                operations[i] = min(operations[i], operations[j] + (i // j))
+
+    return operations[n] if operations[n] != float('inf') else 0
