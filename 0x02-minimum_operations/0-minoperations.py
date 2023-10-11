0d1daa1b7b49c28c12a92b78879c0f42c4055c61
NavanMapp <navanmapp@gmail.com>
2023-10-11 10:16:51 +0000

    file update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 
 def minOperations(n):
+    """
+    Calculate the fewest number of operations needed to result
+    in exactly n H characters in the file.
+
+    :param n: The target number of H characters.
+    :return: The minimum number of operations required.
+    """
     if n <= 1:
         return 0
 
-    operations = [0] * (n + 1)
+    operations = 0
+    divisor = 2
 
-    for i in range(2, n + 1):
-        operations[i] = float('inf')
-        for j in range(1, i):
-            if i % j == 0:
-                operations[i] = min(operations[i], operations[j] + (i // j))
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
 
-    return operations[n] if operations[n] != float('inf') else 0
+    return operations
