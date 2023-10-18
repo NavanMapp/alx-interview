00ddd35c13d026b2ceaa0cdc447226766ed43715
NavanMapp <navanmapp@gmail.com>
2023-10-18 07:36:43 +0000

    log_pasing file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-
 """
 Write a script that reads stdin line by line and computes metrics:
 """
 
 def print_stats(size, status_codes):
-    """Print accumulated metrics.
-
+    """
+    Print accumulated metrics.
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
     """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key])
+        print("{}: {}".format(key, status_codes[key]))
 
 if __name__ == "__main__":
     import sys
